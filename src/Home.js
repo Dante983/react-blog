6eb16536227bbb27383a53410e3601a144847ff7
nikola <savicn209@gmail.com>
2023-06-01 20:30:43 +0200

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const Home = () => {
+    // let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = (e) => {
+        setName('luigi');
+        setAge(30);
+    };
+
+    return ( 
+        <div className="home">
+            <h2>Homepage</h2>
+            <p>{ name } is { age }</p>
+            <button onClick={handleClick}>Click Me</button>
+        </div>
+     );
+}
+ 
+export default Home;
